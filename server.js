fc55c26c21cb1f821b177b98a3c305423edf021a
user <user@ELPBLR-040.evolvus.com>
2018-02-28 10:42:34 +0530

    commented code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -21,10 +22,10 @@ app.use((req, res, next) => {
   });
   next();
 });
-
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
